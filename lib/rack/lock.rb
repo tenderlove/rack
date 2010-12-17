4c491f7d5790a393e1f82e08e3c882c8ea46f16c
Aaron Patterson <aaron.patterson@gmail.com>
2010-12-16 18:47:28 -0800

    unlocking mutex when close is called on the body. closes #87

--- lib/rack/lock.rb
+++ lib/rack/lock.rb
@@ -4,13 +4,30 @@ module Rack
   class Lock
     FLAG = 'rack.multithread'.freeze
 
-    def initialize(app, lock = Mutex.new)
-      @app, @lock = app, lock
+    def initialize(app, mutex = Mutex.new)
+      @app, @mutex = app, mutex
     end
 
     def call(env)
       old, env[FLAG] = env[FLAG], false
-      @lock.synchronize { @app.call(env) }
+
+      # Capture the mutex for the closure below
+      mutex = @mutex
+      mutex.lock
+      response     = @app.call(env)
+      body         = response[2]
+      should_super = body.respond_to?(:close)
+
+      body.extend Module.new {
+        define_method(:close) do
+          begin
+            super() if should_super
+          ensure
+            mutex.unlock
+          end
+        end
+      }
+      response
     ensure
       env[FLAG] = old
     end
